7396248035c1126e9574df5e52a510156799fd41
Miguel Rocha <miguelrocha.dev@gmail.com>
2022-06-05 01:47:56 +0100

    change origin

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL || 'https://thawing-citadel-77165.herokuapp.com/',
+  baseURL: process.env.API_URL || 'https://tiodavis-api.herokuapp.com/',
 });
